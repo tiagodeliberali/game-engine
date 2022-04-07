e0077ad582e2195fd09aa4f74fd2e6b0aec4670c
Tiago Deliberali Santos <tiago.deliberali@outlook.com>
2022-04-07 09:10:59 -0300

    remove shader loader

--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,6 @@ module.exports = {
         use: "ts-loader",
         exclude: /node_modules/,
       },
-      {
-        test: /\.(glsl|vs|fs)$/,
-        loader: "ts-shader-loader",
-      },
     ],
   },
   plugins: [
